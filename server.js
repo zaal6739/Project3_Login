6d6b6674770b47b33761a29346bfc64b3b2934a7
Zack <zack1444@gmail.com>
2019-02-15 11:16:47 -0800

    edited for heroku deployment

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var bodyParser = require("body-parser")
 var app = express()
 var mongoose = require("mongoose")
 var port = process.env.PORT || 5000
+var path = require('path')
 
 app.use(bodyParser.json())
 app.use(cors())
@@ -23,7 +24,14 @@ db.once("open", ()=> console.log("we are connected"))
 //express
 var Users = require('./routes/Users')
 app.use('/users', Users)
+//serve static assets if in heroku 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
 
+    app.get('*',(req,res)=> {
+            res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 app.set('port', process.env.PORT || 5000);//added for heroku
 
 
